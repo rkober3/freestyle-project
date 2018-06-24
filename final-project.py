1dba19d7c809ab3c53815a055f8919a1d7f09746
rkober3 <39286814+rkober3@users.noreply.github.com>
2018-06-24 16:46:55 -0400

    Update final-project.py
    Added and validated user inputs for mortgage calculation and calculated mortgage payment.

--- final-project.py
+++ final-project.py
@@ -52,6 +52,68 @@ while True:
     except ValueError:
         bathrooms = input("Please enter a VALID number of bathrooms (max 1 number after decimal) :")
 
+#Enter mortgage inputs for calculation.
+purchase_price = input("Please enter the purchase price of the property: ")
+while True:
+    try:
+        if "." in purchase_price:
+            if len(purchase_price.split(".", 1)[1]) <= 2:
+                purchase_price = float(purchase_price)
+                break
+            else:
+                purchase_price = input("Please enter a VALID purchase price (max 2 numbers after decimal) :")
+        elif type(int(purchase_price)) == int:
+            purchase_price = float(purchase_price)
+            break
+        else:
+            purchase_price = input("Please enter a VALID purchase price (max 2 numbers after decimal) :")
+    except ValueError:
+        purchase_price = input("Please enter a VALID purchase price (max 2 numbers after decimal) :")
+
+down = input("Please enter the percent of the purchase price to put down: ")
+while True:
+    try:
+        if float(down) <= 1 and float(down) >= 0:
+            down = float(down)
+            break
+        else:
+            down = input("Please enter the percentage down as a decimal between 0 and 1: ")
+    except ValueError:
+        down = input("Please enter the percentage down as a decimal between 0 and 1: ")
+
+rate = input("Please enter the mortgage rate: ")
+while True:
+    try:
+        if float(rate) <= 1 and float(rate) >= 0:
+            rate = float(rate)
+            break
+        else:
+            rate = input("Please enter the mortgage rate as a decimal between 0 and 1: ")
+    except ValueError:
+        down = input("Please enter the mortgage rate as a decimal between 0 and 1: ")
+
+term = input("Please enter the duration of the loan in years (integers only): ")
+while True:
+    try:
+        if type(int(term)) == int:
+            term = float(term)
+            break
+        else:
+            term = input("Please enter a VALID duration for the mortgage in years (integers only): ")
+    except ValueError:
+        term = input("Please enter a VALID duration for the mortgage in years (integers only): ")
+
+principal = purchase_price*(1-down)
+interest = rate
+
+#found on https://code.sololearn.com/c5kHd8o29UHG/#py
+def monthly_loan(principal,interest_rate,duration):
+    n = duration*12 #total number of months
+    r = interest_rate/(100*12) #interest per month
+    monthly_payment = principal*((r*((r+1)**n))/(((r+1)**n)-1)) #formula for compound interest applied on mothly payments.
+    return monthly_payment
+payment = monthly_loan(principal,interest,term)        
+        
 load_dotenv()
 api_key = os.environ.get("MY_API_KEY") or "OOPS. Please set an environment variable named 'MY_API_KEY'."
 f = { 'address' : address, 'bathrooms' : bathrooms,'bedrooms':bedrooms}
